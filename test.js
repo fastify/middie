b81672519b844b1937bc7fe9c047b1f3e7057e73
delvedor <tommydelved@gmail.com>
2017-03-25 00:12:04 +0100

    Updated test

--- test.js
+++ test.js
@@ -13,7 +13,9 @@ test('use no function', t => {
     t.equal(b, res)
   })
 
-  const req = {}
+  const req = {
+    url: '/test'
+  }
   const res = {}
 
   instance.run(req, res)
@@ -27,7 +29,9 @@ test('use a function', t => {
     t.equal(a, req)
     t.equal(b, res)
   })
-  const req = {}
+  const req = {
+    url: '/test'
+  }
   const res = {}
 
   t.equal(instance.use(function (req, res, next) {
@@ -46,7 +50,9 @@ test('use two functions', t => {
     t.equal(a, req)
     t.equal(b, res)
   })
-  const req = {}
+  const req = {
+    url: '/test'
+  }
   const res = {}
   var counter = 0
 
@@ -67,7 +73,9 @@ test('stop the middleware chain if one errors', t => {
   const instance = middie(function (err, a, b) {
     t.ok(err, 'error is forwarded')
   })
-  const req = {}
+  const req = {
+    url: '/test'
+  }
   const res = {}
 
   instance.use(function (req, res, next) {
@@ -76,6 +84,92 @@ test('stop the middleware chain if one errors', t => {
     t.fail('this should never be called')
     next()
   })
+
+  instance.run(req, res)
+})
+
+test('run restricted by path', t => {
+  t.plan(9)
+
+  const instance = middie(function (err, a, b) {
+    t.error(err)
+    t.equal(a, req)
+    t.equal('/test', req.url)
+    t.equal(b, res)
+  })
+  const req = {
+    url: '/test'
+  }
+  const res = {}
+
+  t.equal(instance.use(function (req, res, next) {
+    t.ok('function called')
+    next()
+  }), instance)
+
+  t.equal(instance.use('/test', function (req, res, next) {
+    t.ok('function called')
+    next()
+  }), instance)
+
+  t.equal(instance.use('/no-call', function (req, res, next) {
+    t.fail('should not call this function')
+    next()
+  }), instance)
+
+  instance.run(req, res)
+})
+
+test('run restricted by array path', t => {
+  t.plan(9)
+
+  const instance = middie(function (err, a, b) {
+    t.error(err)
+    t.equal(a, req)
+    t.equal('/test', req.url)
+    t.equal(b, res)
+  })
+  const req = {
+    url: '/test'
+  }
+  const res = {}
+
+  t.equal(instance.use(function (req, res, next) {
+    t.ok('function called')
+    next()
+  }), instance)
+
+  t.equal(instance.use(['/test', '/other-path'], function (req, res, next) {
+    t.ok('function called')
+    next()
+  }), instance)
+
+  t.equal(instance.use(['/no-call', 'other-path'], function (req, res, next) {
+    t.fail('should not call this function')
+    next()
+  }), instance)
+
+  instance.run(req, res)
+})
+
+test('Should strip the url to only match the pathname', t => {
+  t.plan(6)
+
+  const instance = middie(function (err, a, b) {
+    t.error(err)
+    t.equal(a, req)
+    t.equal('/test#foo?bin=baz', req.url)
+    t.equal(b, res)
+  })
+  const req = {
+    url: '/test#foo?bin=baz'
+  }
+  const res = {}
+
+  t.equal(instance.use('/test', function (req, res, next) {
+    t.pass('function called')
+    next()
+  }), instance)
 
   instance.run(req, res)
 })
