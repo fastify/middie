e7389629665c90c21b2e571a649b8e22599855a9
delvedor <tommydelved@gmail.com>
2017-08-23 20:07:47 +0200

    Updated test

--- test.js
+++ test.js
@@ -205,3 +205,25 @@ test('Should strip the url to only match the pathname', t => {
 
   instance.run(req, res)
 })
+
+test('should keep the context', t => {
+  t.plan(6)
+
+  const instance = middie(function (err, a, b) {
+    t.error(err)
+    t.equal(a, req)
+    t.equal(b, res)
+    t.ok(this.key)
+  })
+  const req = {
+    url: '/test'
+  }
+  const res = {}
+
+  t.equal(instance.use(function (req, res, next) {
+    t.pass('function called')
+    next()
+  }), instance)
+
+  instance.run.call({ key: true }, req, res)
+})
